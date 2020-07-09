65b130b02dfea5937596fa69cd60ef7467a44227
kekemuri112 <kekemuri112@naver.com>
2020-07-09 16:27:10 +0900

    coimmit 0709

--- src/main/java/com/jhta/projectdb/controller/MovieController.java
+++ src/main/java/com/jhta/projectdb/controller/MovieController.java
@@ -1,11 +1,14 @@
 package com.jhta.projectdb.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jhta.projectdb.service.GenreService;
@@ -29,10 +32,10 @@ public class MovieController {
 	}
 	
 	@RequestMapping(value="/movieinfo/moviebuyOk.do",produces = {MediaType.APPLICATION_JSON_UTF8_VALUE,MediaType.APPLICATION_XML_VALUE})
-	public String moviebuyok(@RequestBody FilmVo fvo,@RequestBody MovieImgVo mvo,@RequestBody String[] human) {
+	public String moviebuyok(@RequestBody FilmVo fvo,@RequestBody MovieImgVo mvo,@RequestBody HashMap<String, String> map) {
 		try {
 			System.out.println("rest접근.."+fvo.getFilmName());
-			buyService.moviebuyservice(fvo, mvo, human);
+			//buyService.moviebuyservice(fvo, mvo, human);
 			return "success";
 		}catch(Exception e) {
 			return "error";
