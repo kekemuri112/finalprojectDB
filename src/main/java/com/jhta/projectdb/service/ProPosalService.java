0decb87d1e36f8574c57c8b7f03b2ee151920877
woong <gig9150@naver.com>
2020-07-20 18:19:26 +0900

    07/20

---
+++ src/main/java/com/jhta/projectdb/service/ProPosalService.java
@@ -0,0 +1,17 @@
+package com.jhta.projectdb.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jhta.projectdb.dao.ProPosalDao;
+import com.jhta.projectdb.vo.ProposalVo;
+
+@Service
+public class ProPosalService {
+	@Autowired
+	private ProPosalDao dao;
+	
+	public int insert(ProposalVo vo) {
+		return dao.insert(vo);
+	}
+}
